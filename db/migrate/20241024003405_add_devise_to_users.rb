b96b783a3aa4707e44dc8f16f82f4480cc725028
TatsuyaMiyagawa <181440097+miya3928@users.noreply.github.com>
2024-10-29 02:39:34 +0000

    [fix]adduserテーブルの修正

--- db/migrate/20241024003405_add_devise_to_users.rb
+++ db/migrate/20241024003405_add_devise_to_users.rb
@@ -1,16 +1,16 @@
 # frozen_string_literal: true
 
-class DeviseCreateUsers < ActiveRecord::Migration[6.1]
+class AddDeviseToUsers < ActiveRecord::Migration[6.1]
   def self.up
-    create_table :users do |t|
+    change_table :users do |t|
       ## Database authenticatable
      # t.string :email,              null: false, default: ""
      # t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
-      t.string   :reset_password_token
-      t.datetime :reset_password_sent_at
-      t.string   :introduction
+      #t.string   :reset_password_token
+      #t.datetime :reset_password_sent_at
+      #t.string   :introduction
 
 
       ## Rememberable
